4e55597f413ad2dc30b98bf638be706d325f9c61
Rudra2125 <rudra25parmeshwar@gmail.com>
2025-08-08 14:02:42 +0530

    added services for appwrite

---
+++ src/appwrite/auth.js
@@ -0,0 +1,59 @@
+import config from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+    client = new Client();
+    account;
+
+
+    constructor(){
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password,name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if(userAccount){
+                // Call another method
+                return this.login({email,password})
+            }else{
+                return userAccount;
+            }
+        } catch (error) {
+            console.log("Appwrite Service :: Create Account error ", error);
+        }
+    }
+
+    async login({email,password}){
+        try {
+            return  await this.account.createEmailPasswordSession(email,password);
+        } catch (error) {
+            console.log("Appwrite Service :: Login Account error ", error);
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite Service :: Get User error ", error);
+        }
+
+        return null 
+    }
+
+    async logout(){
+        try {
+            await this.account.deleteSessions('');
+        } catch (error) {
+            console.log("Appwrite Service :: Logout error ", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
